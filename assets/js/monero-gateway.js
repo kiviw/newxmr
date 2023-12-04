62423e70936a0d74436844559c18978d031d8090
kiviw <100179542+kiviw@users.noreply.github.com>
2023-12-04 15:05:54 +0300

    Create monero-gateway.js

---
+++ assets/js/monero-gateway.js
@@ -0,0 +1,13 @@
+/**
+ * Monero Gateway JavaScript
+ */
+
+(function ($) {
+    // Add your custom JavaScript logic for the Monero Payment Gateway here.
+    $(document).ready(function () {
+        // Example: You can add event listeners or perform actions when the document is ready.
+    });
+
+    // Additional functions or logic can be added as needed.
+
+})(jQuery);
